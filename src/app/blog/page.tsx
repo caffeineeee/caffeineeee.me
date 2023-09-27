200d9878433279bb52a64fa37ccedd1cab608332
itscevinsam <cevinsam11@gmail.com>
2023-09-27 23:17:26 +0700

    Update styling of nav, add pages

---
+++ src/app/blog/page.tsx
@@ -0,0 +1,22 @@
+"use client";
+
+import { Shell } from "@/components/shells/shell";
+import { usePathname } from "next/navigation";
+
+export default function BlogPage() {
+
+    const currentTab = usePathname();
+    return (
+        <Shell className="gap-12">
+            <section
+                id="hero"
+                aria-labelledby="hero-heading"
+                className="mx-auto flex w-full max-w-[64rem] flex-col items-center justify-center gap-4 pb-8 pt-6 text-center md:pb-12 md:pt-10 lg:py-28"
+            >
+                <h1 className="text-3xl font-bold leading-tight tracking-tighter md:text-5xl lg:text-6xl lg:leading-[1.1]">
+                    {currentTab}
+                </h1>
+            </section>
+        </Shell>
+    );
+}
--- src/components/layouts/site-header.tsx
+++ src/components/layouts/site-header.tsx
@@ -1,11 +1,50 @@
+"use client";
+
 import { ThemeToggle } from "./theme-toggle";
+import { usePathname } from 'next/navigation';
+import { cn } from "@/lib/utils";
 
 export function SiteHeader() {
+
+  const currentTab = usePathname();
 
   return (
     <header className="sticky top-0 z-50 w-full border-b bg-background">
       <div className="container flex h-16 items-center">
-        Test
+        <span className="space-x-4">
+          <a
+            href="/"
+            className={
+              cn("text-primary/60 hover:text-primary",
+                currentTab === "/" && "font-bold text-primary underline decoration-dashed underline-offset-4")
+            }>
+            home
+          </a>
+          <a
+            href="/projects"
+            className={
+              cn("text-primary/60 hover:text-primary",
+                currentTab === "/projects" && "font-bold text-primary underline decoration-dashed underline-offset-4")
+            }>
+            projects
+          </a>
+          <a
+            href="/blog"
+            className={
+              cn("text-primary/60 hover:text-primary",
+                currentTab === "/blog" && "font-bold text-primary underline decoration-dashed underline-offset-4")
+            }>
+            blog
+          </a>
+          <a
+            href="/guestbook"
+            className={
+              cn("text-primary/60 hover:text-primary",
+                currentTab === "/guestbook" && "font-bold text-primary underline decoration-dashed underline-offset-4")
+            }>
+            guestbook
+          </a>
+        </span>
         <div className="flex flex-1 items-center justify-end space-x-4">
           <nav className="flex items-center space-x-2">
             <ThemeToggle />
