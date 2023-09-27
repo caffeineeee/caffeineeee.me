e614be7d0e455d8116028356056f4106e506e39e
itscevinsam <cevinsam11@gmail.com>
2023-09-27 19:37:28 +0700

    Delete some unnecessities, update homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,10 +12,13 @@ export default function IndexPage() {
         className="mx-auto flex w-full max-w-[64rem] flex-col items-center justify-center gap-4 pb-8 pt-6 text-center md:pb-12 md:pt-10 lg:py-28"
       >
         <h1 className="text-3xl font-bold leading-tight tracking-tighter md:text-5xl lg:text-6xl lg:leading-[1.1]">
-          kepinsam.tech
+          hey, I&apos;m kepinsam👋
         </h1>
         <Balancer className="max-w-[46rem] text-lg text-muted-foreground sm:text-xl">
-          Personal site of Cevin Samuel
+          {new Date().getFullYear() - 2002} yo • he/him
+        </Balancer>
+        <Balancer className="max-w-[46rem] text-lg sm:text-xl">
+          I&apos;m a full-stack web developer and a final-year Informatics undergrad student.
         </Balancer>
       </section>
     </Shell>
--- src/components/layouts/site-header.tsx
+++ src/components/layouts/site-header.tsx
@@ -1,6 +1,3 @@
-import { dashboardConfig } from "@/config/dashboard";
-import { MainNav } from "@/components/layouts/main-nav";
-import { MobileNav } from "@/components/layouts/mobile-nav";
 import { ThemeToggle } from "./theme-toggle";
 
 export function SiteHeader() {
@@ -8,10 +5,7 @@ export function SiteHeader() {
   return (
     <header className="sticky top-0 z-50 w-full border-b bg-background">
       <div className="container flex h-16 items-center">
-        <MainNav />
-        <MobileNav
-          sidebarNavItems={dashboardConfig.sidebarNav}
-        />
+        Test
         <div className="flex flex-1 items-center justify-end space-x-4">
           <nav className="flex items-center space-x-2">
             <ThemeToggle />
