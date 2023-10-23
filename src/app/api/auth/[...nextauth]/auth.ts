e56195d05da260527ae4fd874d1a78b365140750
itscevinsam <cevinsam11@gmail.com>
2023-10-24 00:04:25 +0700

    Bump @t3-oss/env-nextjs up to 0.7.1 version

--- src/app/api/auth/[...nextauth]/auth.ts
+++ src/app/api/auth/[...nextauth]/auth.ts
@@ -1,10 +1,11 @@
+import { type AuthOptions } from "next-auth";
 import GitHubProvider from "next-auth/providers/github";
 import TwitterProvider from "next-auth/providers/twitter";
 // import CredentialsProvider from "next-auth/providers/credentials";
 
 import { env } from "@/env.mjs";
 
-export const authOptions = {
+export const authOptions: AuthOptions = {
     providers: [
         GitHubProvider({
             clientId: env.GITHUB_ID ?? "",
