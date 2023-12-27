d145501a6405382f285d4cd6b5e2c0d6ccdb1ef4
itscevinsam <cevinsam11@gmail.com>
2023-12-27 10:06:18 +0700

    Remove unused params

--- src/app/guestbook/UserSession.tsx
+++ src/app/guestbook/UserSession.tsx
@@ -4,9 +4,7 @@ import { SignIn, SignOut } from "@/app/guestbook/buttons";
 import { useSession } from "next-auth/react";
 import Form from "./form";
 
-type Props = {};
-
-export function UserSession(props: Props) {
+export function UserSession() {
     const { data: session } = useSession();
 
     return (
