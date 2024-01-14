3eb2dc10617f0ea32b5975d5a41a8415265d5d23
caffeineeee <cevinsam11@gmail.com>
2024-01-14 22:37:43 +0700

    Change loading spinner color

--- src/components/LoadingSpinner.tsx
+++ src/components/LoadingSpinner.tsx
@@ -5,8 +5,8 @@ import BarLoader from "react-spinners/BarLoader";
 export function LoadingSpinner() {
 	return (
 		<BarLoader
-			color="#EEEEEE"
-			// className="w-40 bg-neutral-300"
+			// color="#EEEEEE"
+			className="w-40 bg-neutral-300"
 		/>
 	);
 }
