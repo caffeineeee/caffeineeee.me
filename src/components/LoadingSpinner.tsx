ff7bcc0ae96e7171fc1a84c399b10dd57f2301ee
caffeineeee <cevinsam11@gmail.com>
2024-01-14 18:14:52 +0700

    Fix rename Loading Spinner component

--- src/components/LoadingSpinner.tsx
+++ src/components/LoadingSpinner.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import BarLoader from "react-spinners/GridLoader";
+import BarLoader from "react-spinners/BarLoader";
 
 export function LoadingSpinner() {
 	return <BarLoader className="w-40 bg-neutral-300" />;
