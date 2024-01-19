e0ea0f2ddd7db49007ae0cb28e3db87ea114925a
caffeineeee <cevinsam11@gmail.com>
2024-01-19 20:11:11 +0700

    Add 404-not-found page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,46 @@
+"use client";
+
+import Link from "next/link";
+import Image from "next/image";
+import {
+	Card,
+	CardContent,
+	CardDescription,
+	CardHeader,
+	CardTitle,
+} from "@/components/ui/card";
+import { usePathname } from "next/navigation";
+import notFound from "public/notFound.jpg";
+import { Button } from "@/components/ui/button";
+import { Separator } from "@/components/ui/separator";
+
+export default function NotFound() {
+	const pathname = usePathname();
+	return (
+		<div className="flex flex-col items-center">
+			<Card className="m-8 flex max-w-md flex-col items-center justify-center bg-stone-950 text-center">
+				<CardHeader>
+					<CardTitle className="text-2xl font-bold">
+						404 Page not found
+						<Separator className="mt-2 w-full" />
+					</CardTitle>
+					<CardDescription>
+						<p>There is no &ldquo;{pathname}&rdquo; page.</p>
+					</CardDescription>
+				</CardHeader>
+				<CardContent>
+					<Image
+						className="p-2"
+						src={notFound}
+						alt="404 page not found"
+						width={375}
+						height={300}
+					/>
+					<Button type="button" className="">
+						<Link href="/">Return home</Link>
+					</Button>
+				</CardContent>
+			</Card>
+		</div>
+	);
+}
