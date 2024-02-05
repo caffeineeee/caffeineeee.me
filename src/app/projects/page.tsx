6aa11e43065a53de9e64c90f9702d2efb4d07cb3
caffeineeee <cevinsam11@gmail.com>
2024-02-05 20:36:54 +0700

    Add Suspense to /projects page

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,12 +1,14 @@
 import { cn } from "@/lib/utils";
 import Link from "next/link";
+import { Suspense } from "react";
+import { Skeleton } from "@/components/ui/skeleton";
 
 export default function ProjectsPage() {
 	const urlStyle =
 		"font-mono dark:text-neutral-300 text-neutral-600/90 dark:hover:text-neutral-50 hover:text-neutral-900 underline underline-offset-2 hover:underline-offset-4 decoration-1 decoration-dashed decoration-rose-500 cursor-pointer";
 
 	return (
-		<div className="container grid items-center gap-12 pb-8 pt-6 md:py-8">
+		<div className="container grid items-center gap-6 pb-8 pt-6 md:py-8">
 			<section
 				id="hero"
 				aria-labelledby="hero-heading"
@@ -16,77 +18,94 @@ export default function ProjectsPage() {
 					projects
 				</h1>
 			</section>
-			<div className="grid xs:grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 tracking-tight dark:text-neutral-300 text-neutral-900">
-				<div className="p-4 flex flex-col text-center">
-					<p className="font-medium">hacker-news-clone</p>
-					<span className="flex flex-row justify-center space-x-4 pb-2">
-						<Link
-							href="https://github.com/caffeineeee/hacker-news-clone"
-							target="_blank"
-							rel="noopener noreferrer nofollow"
-							className={cn(urlStyle)}
-						>
-							github
-						</Link>
-						<Link
-							href="https://hn.caffeineeee.me"
-							target="_blank"
-							rel="noopener noreferrer nofollow"
-							className={cn(urlStyle)}
-						>
-							site
-						</Link>
-					</span>
-					<p className="leading-5">
-						Hacker News clone made with Next14, Tailwind.
-					</p>
+			<Suspense
+				fallback={
+					<div className="flex flex-col space-y-4 justify-center">
+						<Skeleton className="h-3 w-14" />
+						<span className="flex flex-row pb-4">
+							<Skeleton className="h-2 w-8" />
+							<Skeleton className="h-2 w-8" />
+						</span>
+						<span className="flex flex-col justify-center">
+							<Skeleton className="h-3 w-24" />
+							<Skeleton className="h-3 w-24" />
+							<Skeleton className="h-3 w-16" />
+						</span>
+					</div>
+				}
+			>
+				<div className="grid xs:grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 tracking-tight dark:text-neutral-300 text-neutral-900">
+					<div className="p-4 flex flex-col text-center">
+						<p className="font-medium">hacker-news-clone</p>
+						<span className="flex flex-row justify-center space-x-4 pb-2">
+							<Link
+								href="https://github.com/caffeineeee/hacker-news-clone"
+								target="_blank"
+								rel="noopener noreferrer nofollow"
+								className={cn(urlStyle)}
+							>
+								github
+							</Link>
+							<Link
+								href="https://hn.caffeineeee.me"
+								target="_blank"
+								rel="noopener noreferrer nofollow"
+								className={cn(urlStyle)}
+							>
+								site
+							</Link>
+						</span>
+						<p className="leading-5">
+							Hacker News clone made with Next14, Tailwind.
+						</p>
+					</div>
+					<div className="p-4 flex flex-col text-center">
+						<p className="font-medium">
+							Batak Toba language-Indonesian machine translation
+						</p>
+						<span className="flex flex-row justify-center space-x-4 pb-2">
+							<Link
+								href="https://huggingface.co/kepinsam/batak_toba_to_indonesian_translation_model"
+								className={cn(urlStyle)}
+								target="_blank"
+								rel="noopener noreferrer nofollow"
+							>
+								bbc→ind
+							</Link>
+							<Link
+								href="https://huggingface.co/kepinsam/indonesian_to_batak_toba_translation_model"
+								className={cn(urlStyle)}
+								target="_blank"
+								rel="noopener noreferrer nofollow"
+							>
+								ind→bbc
+							</Link>
+						</span>
+						<p className="leading-5">
+							Like Google Translate, but for Batak Toba and Indonesian
+							language-pair, cuz there is no option for Batak Toba in Google
+							Translate.
+						</p>
+					</div>
+					<div className="p-4 flex flex-col text-center">
+						<p className="font-medium">caffeineeee.me</p>
+						<span className="flex flex-row justify-center space-x-4 pb-2">
+							<Link
+								href="https://github.com/caffeineeee/caffeineeee.me"
+								className={cn(urlStyle)}
+								target="_blank"
+								rel="noopener noreferrer nofollow"
+							>
+								github
+							</Link>
+						</span>
+						<p className="leading-5">
+							Yet another personal site of mine made with Next14, Tailwind,
+							ShadcnUI, NextAuth, DrizzleORM, Turso SQLite.
+						</p>
+					</div>
 				</div>
-				<div className="p-4 flex flex-col text-center">
-					<p className="font-medium">
-						Batak Toba language-Indonesian machine translation
-					</p>
-					<span className="flex flex-row justify-center space-x-4 pb-2">
-						<Link
-							href="https://huggingface.co/kepinsam/batak_toba_to_indonesian_translation_model"
-							className={cn(urlStyle)}
-							target="_blank"
-							rel="noopener noreferrer nofollow"
-						>
-							bbc→ind
-						</Link>
-						<Link
-							href="https://huggingface.co/kepinsam/indonesian_to_batak_toba_translation_model"
-							className={cn(urlStyle)}
-							target="_blank"
-							rel="noopener noreferrer nofollow"
-						>
-							ind→bbc
-						</Link>
-					</span>
-					<p className="leading-5">
-						Like Google Translate, but for Batak Toba and Indonesian
-						language-pair, cuz there is no option for Batak Toba in Google
-						Translate.
-					</p>
-				</div>
-				<div className="p-4 flex flex-col text-center">
-					<p className="font-medium">caffeineeee.me</p>
-					<span className="flex flex-row justify-center space-x-4 pb-2">
-						<Link
-							href="https://github.com/caffeineeee/caffeineeee.me"
-							className={cn(urlStyle)}
-							target="_blank"
-							rel="noopener noreferrer nofollow"
-						>
-							github
-						</Link>
-					</span>
-					<p className="leading-5">
-						Yet another personal site of mine made with Next14, Tailwind,
-						ShadcnUI, NextAuth, DrizzleORM, Turso SQLite.
-					</p>
-				</div>
-			</div>
+			</Suspense>
 		</div>
 	);
 }
