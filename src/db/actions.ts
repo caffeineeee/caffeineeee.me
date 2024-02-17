b77f352d52504c4db4c794cec3a46aa7fe4329ff
caffeineeee <cevinsam11@gmail.com>
2024-02-17 08:46:40 +0700

    Replace cv to latest, add more return statements when error

--- src/db/actions.ts
+++ src/db/actions.ts
@@ -34,48 +34,30 @@ export async function insertGuestbookEntry(formData: FormData) {
 	const body = entry.slice(0, 500);
 
 	try {
-		await db.insert(guestbook).values({
+		const insertedEntry = await db.insert(guestbook).values({
 			id: urlId,
 			email: email,
 			body: body,
 			created_by: created_by,
 			created_at: sql`CURRENT_TIMESTAMP`,
 		});
+		revalidatePath("/guestbook");
+
+		return insertedEntry;
 	} catch (error) {
 		console.error("Error from insertGuestbookEntry: ", error);
+		return null;
 	}
-
-	revalidatePath("/guestbook");
-
-	const transporter = nodemailer.createTransport({
-		service: "gmail",
-		auth: {
-			user: "cevinsam11@gmail.com",
-			pass: process.env.GOOGLE_APP_PASSWORD,
-		},
-	});
-
-	const mailOptions = {
-		from: "cevinsam11@gmail.com",
-		to: "cevin.samuel@yahoo.com",
-		subject: "New Guestbook Entry",
-		html: `<p>Email: '${email}'</p><p>Name: '${name}'</p><p>Profile image URL: '${image}'</p><p>Message: '${body}'</p>`,
-	};
-
-	transporter.sendMail(mailOptions, (error, info) => {
-		if (error) {
-			throw new Error(error.message);
-		}
-		console.log("Email sent", info.response);
-	});
 }
 
 export async function deleteOwnGuestbookEntries(id: string) {
 	try {
-		await db.delete(guestbook).where(eq(guestbook.id, id));
+		const deletedEntry = await db.delete(guestbook).where(eq(guestbook.id, id));
+		revalidatePath("/guestbook");
+
+		return deletedEntry;
 	} catch (error) {
 		console.error("Error from deleteOwnGuestbookEntries: ", error);
+		return null;
 	}
-
-	revalidatePath("/guestbook");
 }
