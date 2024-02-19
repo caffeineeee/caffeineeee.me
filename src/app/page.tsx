b611fa01daf1d8ee092fc633d2b5a7b863c08533
caffeineeee <cevinsam11@gmail.com>
2024-02-20 06:14:32 +0700

    Remove personal info

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,3 @@
-import { differenceInYears } from "date-fns";
 import { revalidatePath } from "next/cache";
 
 export default function IndexPage() {
@@ -6,8 +5,6 @@ export default function IndexPage() {
 	setInterval(() => {
 		revalidatePath("/", "layout"); // revalidating all data. See: https://nextjs.org/docs/app/api-reference/functions/revalidatePath#revalidating-all-data
 	}, intervDuration);
-
-	const myYearsOfAge = differenceInYears(new Date(), new Date(2002, 2, 11)); // Note: Months are 0-indexed in JavaScript, so March is 2.
 
 	return (
 		<div className="container grid items-center gap-12 pb-8 pt-6 md:py-8">
@@ -19,9 +16,6 @@ export default function IndexPage() {
 				<h1 className="text-4xl font-bold leading-tight tracking-tighter md:text-5xl lg:text-6xl lg:leading-[1.1] font-display">
 					hey, I&apos;m caffeineeee👋
 				</h1>
-				<div className="max-w-[46rem] text-lg text-muted-foreground sm:text-xl">
-					{myYearsOfAge} yo • he/him
-				</div>
 				<div className="max-w-[46rem] text-lg sm:text-xl">
 					I&apos;m a full-stack web developer and a final-year Informatics
 					undergrad student.
